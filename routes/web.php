762bd2c211b2cd38a24d6784324e5791ca55acaf
Sakeerin Khami <sakeerin.k@vbeyond.co.th>
2025-06-21 14:57:08 +0700

    generate model, migrate and generate resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
